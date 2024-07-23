1a45d7528c584304b8605625890c966ee8f8ab4b
juseong <wjdwntjd55@naver.com>
2024-07-23 17:00:24 +0900

    feat : bottomNavigation 구현

--- app/src/main/java/com/simply407/patpat/MainActivity.kt
+++ app/src/main/java/com/simply407/patpat/MainActivity.kt
@@ -5,16 +5,48 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.simply407.patpat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        bottomNavigation()
+    }
+
+    fun bottomNavigation() {
+        binding.bottomNavigationViewMain.run {
+
+            itemIconTintList = null
+
+            setOnItemSelectedListener {
+                when (it.itemId) {
+                    R.id.home_menu_item -> {
+                        return@setOnItemSelectedListener true
+                    }
+
+                    R.id.inbox_menu_item -> {
+                        return@setOnItemSelectedListener true
+                    }
+
+                    R.id.user_menu_item -> {
+                        return@setOnItemSelectedListener true
+                    }
+
+                    else -> return@setOnItemSelectedListener false
+                }
+            }
         }
     }
 }

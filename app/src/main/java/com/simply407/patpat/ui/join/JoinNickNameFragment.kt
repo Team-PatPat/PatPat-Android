86a4fcd72a300ab36f99719b96f15c0e858c7b74
juseong <wjdwntjd55@naver.com>
2024-07-24 21:53:41 +0900

    feat : 닉네임 입력 화면

---
+++ app/src/main/java/com/simply407/patpat/ui/join/JoinNickNameFragment.kt
@@ -0,0 +1,57 @@
+package com.simply407.patpat.ui.join
+
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.simply407.patpat.R
+import com.simply407.patpat.databinding.FragmentJoinNickNameBinding
+
+class JoinNickNameFragment : Fragment() {
+
+    private lateinit var joinInfoActivity: JoinInfoActivity
+    private lateinit var binding: FragmentJoinNickNameBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentJoinNickNameBinding.inflate(layoutInflater, container, false)
+
+        checkValidText()
+
+        return binding.root
+    }
+
+    fun checkValidText() {
+        binding.textInputLayoutJoinNickName.editText?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // Do nothing
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                s?.let {
+                    if (it.length == 15) {
+                        binding.textInputLayoutJoinNickName.helperText = "입력은 최대 15자까지 가능합니다."
+                    } else {
+                        binding.textInputLayoutJoinNickName.helperText = null
+                    }
+
+                    if (it.isEmpty()) {
+                        binding.buttonCheckJoinComplete.setBackgroundResource(R.drawable.bg_gray_r15)
+                    } else{
+                        binding.buttonCheckJoinComplete.setBackgroundResource(R.drawable.bg_orange_r15)
+                    }
+                }
+            }
+        })
+    }
+
+}

dd99ba11c91e5049245601e81431f5c7ed63ad1f
juseong <wjdwntjd55@naver.com>
2024-07-26 17:24:12 +0900

    Remove build directory from version control

--- app/build/generated/data_binding_trigger/debug/com/simply407/patpat/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.simply407.patpat;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
--- app/build/generated/source/kapt/debug/com/simply407/patpat/BR.java
+++
@@ -1,5 +0,0 @@
-package com.simply407.patpat;
-
-public class BR {
-  public static final int _all = 0;
-}

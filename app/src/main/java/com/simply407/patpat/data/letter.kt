9280da33c9b1c6f8d4a90a4774cdcd665d08b0cd
윤성준 <1__________999@naver.com>
2024-07-27 14:09:12 +0900

    feat : letterxml 추가

---
+++ app/src/main/java/com/simply407/patpat/data/letter.kt
@@ -0,0 +1,9 @@
+package com.simply407.patpat.data
+
+data class letter (
+    val counselor : String,
+    val username : String,
+    val content : String,
+    val commentImage: Int?,
+    val commentString : String?
+)

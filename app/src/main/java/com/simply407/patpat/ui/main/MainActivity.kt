afea2bca6a7a089285c478a89d94ad1aeeae65b7
juseong <wjdwntjd55@naver.com>
2024-07-28 03:59:34 +0900

    design : 스플래시, 온보딩, 로그인 화면 디자인 변경

--- app/src/main/java/com/simply407/patpat/ui/main/MainActivity.kt
+++ app/src/main/java/com/simply407/patpat/ui/main/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main1)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
 
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)

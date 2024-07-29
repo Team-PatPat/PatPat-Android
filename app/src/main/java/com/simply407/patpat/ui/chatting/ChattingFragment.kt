3bb6c57b0bc92f134e0456e7c3056cf04cd644b4
juseong <wjdwntjd55@naver.com>
2024-07-29 14:56:25 +0900

    feat : 메시지 입력하는 곳 ui 구현

--- app/src/main/java/com/simply407/patpat/ui/chatting/ChattingFragment.kt
+++ app/src/main/java/com/simply407/patpat/ui/chatting/ChattingFragment.kt
@@ -1,6 +1,8 @@
 package com.simply407.patpat.ui.chatting
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -50,6 +52,20 @@ class ChattingFragment : Fragment() {
                     }
                 }
             }
+
+            editTextChatting.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    if (s.isNullOrEmpty()) {
+                        buttonChatting.setImageResource(R.drawable.ic_send_off)
+                    } else {
+                        buttonChatting.setImageResource(R.drawable.ic_send_on)
+                    }
+                }
+
+                override fun afterTextChanged(s: Editable?) {}
+            })
         }
     }
 

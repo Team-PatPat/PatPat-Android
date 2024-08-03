90be0a7c67ee074e09099f900cf2a642a337ecf5
juseong <wjdwntjd55@naver.com>
2024-08-04 05:03:51 +0900

    feat : Switch 버튼 디자인 구현

--- app/src/main/java/com/simply407/patpat/ui/storagebox/StorageBoxFragment.kt
+++ app/src/main/java/com/simply407/patpat/ui/storagebox/StorageBoxFragment.kt
@@ -1,21 +1,55 @@
 package com.simply407.patpat.ui.storagebox
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.simply407.patpat.R
+import com.simply407.patpat.databinding.FragmentStorageBoxBinding
+import com.simply407.patpat.ui.main.MainActivity
 
 
 class StorageBoxFragment : Fragment() {
+
+    private lateinit var mainActivity: MainActivity
+    private lateinit var binding: FragmentStorageBoxBinding
+
+    val TAG = "StorageBoxFragment"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_storage_box, container, false)
+
+        mainActivity = activity as MainActivity
+        binding = FragmentStorageBoxBinding.inflate(layoutInflater, container, false)
+
+        initUi()
+
+        return binding.root
+    }
+
+    private fun initUi() {
+        binding.run {
+
+            switchStorageBox.run {
+                thumbTintList = ContextCompat.getColorStateList(requireContext(), R.color.orange)
+                trackTintList = ContextCompat.getColorStateList(requireContext(), R.color.grayScale200)
+
+                setOnCheckedChangeListener { _, isChecked ->
+                    if (isChecked) {
+                        textViewAllStorageBox.setTextColor(ContextCompat.getColor(requireContext(), R.color.grayScale400))
+                        textViewFavoriteStorageBox.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
+                    } else {
+                        textViewAllStorageBox.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
+                        textViewFavoriteStorageBox.setTextColor(ContextCompat.getColor(requireContext(), R.color.grayScale400))
+                    }
+                }
+            }
+        }
     }
 
 
